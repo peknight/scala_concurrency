08d6a83b7eae9b2070e12149072fea1b14c8f8fe
peknight <peknight@qq.com>
2018-12-10 22:40:32 +0800

    update ch7

--- src/main/scala/org/learningconcurrency/ch7/TSortedList.scala
+++ src/main/scala/org/learningconcurrency/ch7/TSortedList.scala
@@ -12,16 +12,42 @@ class TSortedList {
   }
 
   import scala.annotation.tailrec
+  @tailrec
+  final def insert(n: Node, x: Int)(implicit txn: InTxn): Unit = {
+    if (n.next() == null || n.next().elem > x) {
+      n.append(new Node(x, Ref[Node](null)))
+    } else insert(n.next(), x)
+  }
+
   def insert(x: Int): this.type = atomic {
     implicit txn =>
-      @tailrec def insert(n: Node): Unit = {
-        if (n.next() == null || n.next().elem > x) {
-          n.append(new Node(x, Ref[Node](null)))
-        } else insert(n.next())
-      }
+//      @tailrec def insert(n: Node): Unit = {
+//        if (n.next() == null || n.next().elem > x) {
+//          n.append(new Node(x, Ref[Node](null)))
+//        } else insert(n.next())
+//      }
+
       if (head() == null || head().elem > x) {
         head() = new Node(x, Ref(head()))
-      } else insert(head())
+      } else insert(head(), x)
       this
   }
+
+  def pop(xs: TSortedList, n: Int): Unit = atomic {
+    implicit txn =>
+      var left = n
+      while (left > 0) {
+        xs.head() = xs.head().next()
+        left -= 1
+      }
+  }
+}
+object TSortedList extends App {
+  import scala.concurrent._
+  import ExecutionContext.Implicits.global
+  val sortedList = new TSortedList
+  val f = Future { sortedList.insert(1); sortedList.insert(4) }
+  val g = Future { sortedList.insert(2); sortedList.insert(3) }
+  for (_ <- f; _ <- g) log(s"sorted list - $sortedList")
+  Thread.sleep(1000)
 }
